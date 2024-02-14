7477824cebcd1e52e2ca1f63765dd3d205b53755
Maiylsamy D <139238726+maiylsamy@users.noreply.github.com>
2024-02-14 17:45:51 +0530

    python simple logic codes

---
+++ minpj/dict basic.py
@@ -0,0 +1,19 @@
+x = {'name' :'maiyl','course' : 'python' ,'age' : 23 ,'palce' :'bangalore'}
+y = {'fees':50000}
+
+x.update(y)
+print(x)
+
+
+
+x.update (name = 'mosh')p
+print(x)
+
+# ======================
+for i,j in dict.items(x):
+    print(i,j)
+# --------------------------------
+for i in x :
+    print(x[i])
+    
+    
---
+++ minpj/set basics.py
@@ -0,0 +1,7 @@
+x = [1,'loki',234,33]
+x += [10]
+x.append([334,'mass'])
+x.extend([334,'mass',True])
+print(x)
+
+# list allow all value like set , tuple ,dict
